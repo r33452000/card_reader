7f88cfaee06c281c4c4a23eb6674cc2cd7e4c8d6
r33452000 <r332452000@gmail.com>
2020-08-17 12:18:30 +0800

    驗證時暫停攝影

--- v2/app.js
+++ v2/app.js
@@ -4,7 +4,24 @@ new Vue({
   data() {
     return {
       decodedContent: '',
-      errorMessage: ''
+      errorMessage: '',
+      isValid: undefined,
+      camera: 'auto',
+      result: null,
+    }
+  },
+  computed: {
+    validationPending () {
+      return this.isValid === undefined
+        && this.camera === 'off'
+    },
+
+    validationSuccess () {
+      return this.isValid === true
+    },
+
+    validationFailure () {
+      return this.isValid === false
     }
   },
 
@@ -31,7 +48,38 @@ new Vue({
         } else {
           this.errorMessage = 'UNKNOWN ERROR: ' + error.message
         }
+      }).then(this.resetValidationState)
+    },    
+
+    resetValidationState () {
+      this.isValid = undefined
+    },
+    async onDecode (content) {
+      this.result = content
+      this.turnCameraOff()
+
+      // pretend it's taking really long
+      await this.timeout(3000)
+      this.isValid = content.startsWith('http')
+
+      // some more delay, so users have time to read the message
+      await this.timeout(2000)
+
+      this.turnCameraOn()
+    },
+    turnCameraOn () {
+      this.camera = 'auto'
+    },
+
+    turnCameraOff () {
+      this.camera = 'off'
+    },
+
+    timeout (ms) {
+      return new Promise(resolve => {
+        window.setTimeout(resolve, ms)
       })
     }
+
   }
 })
