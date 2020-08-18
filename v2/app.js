72977fddf801118f32625b4131a3ea5f2ab814dc
r33452000 <r332452000@gmail.com>
2020-08-18 17:31:01 +0800

    reset to demo

--- v2/app.js
+++ v2/app.js
@@ -26,40 +26,33 @@ var vm = new Vue({
       return this.isValid === false
     }
   },
-
-  methods: {       
+  
+   methods: {
 
     onInit (promise) {
       promise
         .catch(console.error)
         .then(this.resetValidationState)
-    },  
+    },
 
     resetValidationState () {
       this.isValid = undefined
     },
-    
+
     async onDecode (content) {
-      this.result = content;
-      this.turnCameraOff();
-      enterAsync();
-      this.decodedContent = content;
-      this.record = this.record + 1;
+      this.result = content
+      this.turnCameraOff()
 
       // pretend it's taking really long
-      await this.timeout(200)
-      this.isValid = content.startsWith('http');
+      await this.timeout(3000)
+      this.isValid = content.startsWith('http')
 
-      // some more delay, so users have time to read the message      
-      
-      //playAudio("pink");
-      await this.timeout(800);
+      // some more delay, so users have time to read the message
+      await this.timeout(2000)
 
-      this.turnCameraOn();
+      this.turnCameraOn()
     },
-    enterAsync(){
-    	this.enter = this.enter + 1;
-    },
+
     turnCameraOn () {
       this.camera = 'auto'
     },
@@ -67,16 +60,15 @@ var vm = new Vue({
     turnCameraOff () {
       this.camera = 'off'
     },
-    playAudio(card_key){
-      var lv_audio = new Audio('https://sound-wall.s3-eu-west-1.amazonaws.com/en_' + card_key + '_word.mp3');
-      lv_audio.play();
-    },
 
     timeout (ms) {
       return new Promise(resolve => {
         window.setTimeout(resolve, ms)
       })
     }
-
   }
 });
+
+       
+
+  
