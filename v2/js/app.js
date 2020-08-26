539627036a802c71b7fda7b17b7383f0e73e7400
r33452000 <r332452000@gmail.com>
2020-08-26 17:30:04 +0800

    Update app.js

--- v2/js/app.js
+++ v2/js/app.js
@@ -75,7 +75,7 @@ var vm = new Vue({
 		  
       // some more delay, so users have time to read the message
       //await this.timeout(800)
-      if(this.isVaild){
+      if(this.isValid == true){
       	this.errorMessage = "play https://sound-wall.s3-eu-west-1.amazonaws.com/en_"+lv_item.t+"_word.mp3";
       	await playAudio("https://sound-wall.s3-eu-west-1.amazonaws.com/en_"+lv_item.t+"_word.mp3");
       }else{
