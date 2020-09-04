9d0198751b5932db34648418b251898448c0fd19
r33452000 <r332452000@gmail.com>
2020-09-04 18:25:22 +0800

    catch error to alter

--- v2/js/app.js
+++ v2/js/app.js
@@ -131,6 +131,26 @@ var vm = new Vue({
 window.onload = function () {
 	//alert('讀取到正確條碼會發出聲音！');
 	};
+	
+	window.onerror = function (msg, url, lineNo, columnNo, error) {
+  var string = msg.toLowerCase();
+  var substring = "script error";
+  if (string.indexOf(substring) > -1){
+    alert('Script Error: See Browser Console for Detail');
+  } else {
+    var message = [
+      'Message: ' + msg,
+      'URL: ' + url,
+      'Line: ' + lineNo,
+      'Column: ' + columnNo,
+      'Error object: ' + JSON.stringify(error)
+    ].join(' - ');
+
+    alert(message);
+  }
+
+  return false;
+};
 
        
 
